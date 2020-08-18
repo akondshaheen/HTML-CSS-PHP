02cf8f0ec29b1d3f63db2b3b02f682e5681af469
akondshaheen <akondshaheen.es@gmail.com>
2020-08-19 01:42:46 +0200

    HTML CSS PHP Basic project

---
+++ php/index.php
@@ -0,0 +1,16 @@
+<?php ob_start();
+
+
+if(isset($_POST['submit'])) {
+    $to = "akondshaheen@gmail.com";
+    $subject = $_POST['subject'];
+    $email = $_POST['email'];
+    $txt = $_POST['message']
+    $headers = "From: " .$email . "r\n\".
+    "CC: somebodyelse@example.com";
+    
+    mail($to,$subject,$txt,$headers);
+    header("Location: contact.html");
+    
+    
+}
